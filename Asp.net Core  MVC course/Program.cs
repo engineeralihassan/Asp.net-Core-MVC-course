72436021f46798fc31632bcecf152914df68cf78
Ali Hassan <engineralihassan@gmail.com>
2023-07-11 11:07:39 +0500

    Add project files.

---
+++ Asp.net Core  MVC course/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http;
+
+Console.WriteLine("Asp.Net Core Mvc core previouse covered topics revisons");
+Console.WriteLine("The Get started section points ");
+Console.WriteLine("HTTp Servers");
+Console.WriteLine("Kestral server");
+Console.WriteLine("1-internet <-> Kestral <-> Application code ");
+Console.WriteLine("2-");
+//////////////////////////////
+/// IIS Server
+/// /////////////////////////
+/// using System.Web;
+
+//public class HelloWorld : IHttpHandler
+//{
+//    public void ProcessRequest(HttpContext context)
+//    {
+//        context.Response.ContentType = "text/plain";
+//        context.Response.Write("Hello, IIS!");
+//    }
+
+//    public bool IsReusable => true;
+//}
+//////////////////////////////
+/// Kestral Server
+/// /////////////////////////
+//public static void Main(string[] args)
+//{
+//    var host = new WebHostBuilder()
+//        .UseKestrel()
+//        .Configure(app =>
+//        {
+//            app.Run(context =>
+//            {
+//                return context.Response.WriteAsync("Hello, Kestrel!");
+//            });
+//        })
+//        .Build();
+
+//    host.Run();
+//}
